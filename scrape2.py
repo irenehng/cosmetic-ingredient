abf153911bf7374c7e01501587bdddc2e86372c1
Irene <irene.ng@berkeley.edu>
2023-07-14 16:16:18 +0700

    scraped data

--- scrape2.py
+++ scrape2.py
@@ -2,13 +2,16 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+
 # from selenium.webdriver.support import expected_conditions as EC
 # from selenium.webdriver.support.ui import WebDriverWait
 import csv
 
 driver = webdriver.Chrome()
 
-driver.get("https://ec.europa.eu/growth/tools-databases/cosing/reference/functions/list/ABSORBENT")
+driver.get(
+    "https://ec.europa.eu/growth/tools-databases/cosing/reference/functions/list/pH ADJUSTERS"
+)
 driver.implicitly_wait(30)
 table_data = []
 
@@ -22,7 +25,7 @@ table_data = []
 #         for i in ingredients:
 #             # table_data.append([i.text.strip(), "Depilatory"])
 #             print(i.text)
-        
+
 #         try:
 #             next_button= driver.find_element(By.XPATH,'//a[@aria-label="Go to next page"]')
 #             driver.execute_script("arguments[0].scrollIntoView(true);", next_button)
@@ -37,16 +40,22 @@ table_data = []
 #     time.sleep(2)
 
 
-
 while True:
-    function = driver.find_element(By.XPATH, "/html/body/app-root/ecl-app/main/div/ng-component/h6/strong[3]").text.lower()
-    ingredients = driver.find_elements(By.XPATH,"/html/body/app-root/ecl-app/main/div/ng-component/app-results-subs/table/tbody/tr/td[2]/a")
+    function = driver.find_element(
+        By.XPATH, "/html/body/app-root/ecl-app/main/div/ng-component/h6/strong[3]"
+    ).text.lower()
+    ingredients = driver.find_elements(
+        By.XPATH,
+        "/html/body/app-root/ecl-app/main/div/ng-component/app-results-subs/table/tbody/tr/td[2]/a",
+    )
     for i in ingredients:
-        table_data.append([i.text.strip().lower(), "absorbent"])
+        table_data.append([i.text.strip().lower(), "ph adjusters"])
         print(i.text)
-    
+
     try:
-        next_button= driver.find_element(By.XPATH,'//a[@aria-label="Go to next page"]')
+        next_button = driver.find_element(
+            By.XPATH, '//a[@aria-label="Go to next page"]'
+        )
         driver.execute_script("arguments[0].scrollIntoView(true);", next_button)
         next_button.click()
         time.sleep(10)
